1fbe2b4ab98a27e883a875193231a4bf3f0e0e09
Florian de Vries <florian.devries@cegeka.com>
2024-01-18 09:38:59 +0100

    Add 'Test' tag to resource group

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ resource "azurerm_resource_group" "resource_group" {
   managed_by = var.managed_by
 
   # Assign specified tags and add default `ManagedBy` and `CreationDate` tags.
-  tags = merge(var.tags, { ManagedBy = "Terraform", CreationDate = timestamp() })
+  tags = merge(var.tags, { Test = "Test", ManagedBy = "Terraform", CreationDate = timestamp() })
 
   # Prevent updates to `CreationDate` tag after creation.
   lifecycle {
