9d9eaea6e3676d85194bd0f1d23d28ed68889ec2
Florian de Vries <florian.de.vries@conclusion.nl>
2024-05-29 11:05:18 +0200

    Refactor resource group configuration and add output for resource group name

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,8 @@ output "id" {
   value       = azurerm_resource_group.rg.id
   description = "The ID of the Resource Group."
 }
+
+output "name" {
+  value       = azurerm_resource_group.rg.name
+  description = "The Name of the Resource Group."
+}
