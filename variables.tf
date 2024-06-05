cff1977725bf2fe8b209803cf0b7f91c632e5e61
Florian de Vries <florian.de.vries@conclusion.nl>
2024-06-05 08:56:59 +0200

    Refactor resource group configuration and update README

--- variables.tf
+++ variables.tf
@@ -1,21 +1,21 @@
 variable "name" {
   type        = string
-  description = "The Azure Region where the Resource Group should exist. Changing this forces a new Resource Group to be created."
+  description = "(Required) The Name which should be used for this Resource Group. Changing this forces a new Resource Group to be created."
 }
 
 variable "location" {
   type        = string
-  description = "The Name which should be used for this Resource Group. Changing this forces a new Resource Group to be created."
+  description = "(Required) The Azure Region where the Resource Group should exist. Changing this forces a new Resource Group to be created."
 }
 
 variable "managed_by" {
   type        = string
   default     = null
-  description = "The ID of the resource or application that manages this Resource Group."
+  description = "(Optional) The ID of the resource or application that manages this Resource Group."
 }
 
 variable "tags" {
   type        = any
   default     = null
-  description = "A mapping of tags which should be assigned to the Resource Group."
+  description = "(Optional) A mapping of tags which should be assigned to the Resource Group."
 }
